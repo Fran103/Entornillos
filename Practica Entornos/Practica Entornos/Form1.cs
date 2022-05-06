e54857ad6b94cd46282beeef30ad2a2f2b2c6f2d
SNIdiot <snivanDev@gmail.com>
2022-05-06 20:04:15 +0200

    Update Primera rama y lista de socios

--- Practica Entornos/Practica Entornos/Form1.cs
+++ Practica Entornos/Practica Entornos/Form1.cs
@@ -12,9 +12,24 @@ namespace Practica_Entornos
 {
     public partial class Form1 : Form
     {
+        private List<string> socios = new List<string>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tListaSocios.Text = "";
+            string socioAñadir="";
+            socioAñadir = tNombre.ToString();
+            socios.Add(socioAñadir);
+            string aux = "";
+            for(int i = 0; i < socios.Count; i++)
+            {
+                aux += socios[i] + " ,";
+            }
+            tListaSocios.Text = aux;
         }
     }
 }
