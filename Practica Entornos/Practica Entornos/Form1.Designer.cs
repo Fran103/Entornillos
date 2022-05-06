84f04c5a0ca3e07134d6760d799eb456d3cae9cc
Fran Martinez Garcia <fran_pop10@hotmail.com>
2022-05-06 19:31:14 +0200

    update

--- Practica Entornos/Practica Entornos/Form1.Designer.cs
+++ Practica Entornos/Practica Entornos/Form1.Designer.cs
@@ -29,33 +29,56 @@ namespace Practica_Entornos
         /// </summary>
         private void InitializeComponent()
         {
+            this.tNombre = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.tListaSocios = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // tNombre
+            // 
+            this.tNombre.Location = new System.Drawing.Point(44, 60);
+            this.tNombre.Name = "tNombre";
+            this.tNombre.Size = new System.Drawing.Size(100, 20);
+            this.tNombre.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(319, 75);
+            this.button1.Location = new System.Drawing.Point(215, 60);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Guardar";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tListaSocios
+            // 
+            this.tListaSocios.Location = new System.Drawing.Point(44, 117);
+            this.tListaSocios.Multiline = true;
+            this.tListaSocios.Name = "tListaSocios";
+            this.tListaSocios.ReadOnly = true;
+            this.tListaSocios.Size = new System.Drawing.Size(702, 295);
+            this.tListaSocios.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tListaSocios);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.tNombre);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TextBox tNombre;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tListaSocios;
     }
 }
 
