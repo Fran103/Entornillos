a3b32ef8c7934e309996dfb0ad35f394ed33cebe
Fran Martinez Garcia <fran_pop10@hotmail.com>
2022-05-16 17:41:25 +0200

    entornos

--- Fecha/libFechas/FechaFMG.cs
+++ Fecha/libFechas/FechaFMG.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 
- namespace FechaProg
+namespace FechaProg
 {
-        public class Fecha
-        {
+    public class Fecha
+    {
         private int d;
         private int m;
         private int a;
@@ -19,84 +19,84 @@
         /// Se establecen los valores a 1
         /// </summary>
         public Fecha()
-            {
-                D = 1;
-                M = 1;
-                A = 1;
-            }
-            /// <summary>
-            /// Constructor de Fecha con 3 parámetros
-            /// Si algún parámetro no es correcto se establece a 1
-            /// </summary>
-            /// <param name="mes">Mes</param>
-            /// <param name="anyo">Anyo (entre 1 y 2500)</param>
-            /// <param name="dia">Dia</param>
-            /// <param name="bi">Indica si es bisiesto</param>
-            public Fecha(int mes, int anyo, int dia)
-            {
-                if (anyo >= 1 && anyo <= 2500)
-                {
-                    this.A = anyo;
-                }
-                else
-                {
-                    this.A = 1;
-                }
-                if ((A % 400 == 0) || ((A % 4 == 0) && (A % 100 != 0)))
-                    bisiesto = true;
-                else bisiesto = false;
-                if (mes >= 1 && mes <= 12)
-                    this.M = mes;
-                else
-                    this.M = 1;
-                int diasMes = 0;
-                switch (M)
-                {
-                    case 1:
-                    case 3:
-                    case 5:
-                    case 7:
-                    case 8:
-                    case 10:
-                    case 12:
-                        diasMes = 31;
-                        break;
-                    case 4:
-                    case 6:
-                    case 9:
-                    case 11:
-                        diasMes = 30;
-                        break;
-                    case 2: // verificación de año bisiesto
-                        if (bisiesto)
-                            diasMes = 29;
-                        else
-                            diasMes = 28;
-                        break;
-                }
-                
-            if (dia >= 1 && dia <= diasMes)
-                    this.D = dia;
-                else
-                    this.D = 1;
-            }
-            public bool EsBisiesto()
-            {
-                if ((A % 400 == 0) || ((A % 4 == 0) && (A % 100 != 0)))
-                    bisiesto = true;
-                else
-                    bisiesto = false;
-                return bisiesto;
-            }
-            /// <summary>
-            /// Devuelve un string con la fecha en formato dia/mes/anyo
-            /// </summary>
-            /// <remarks> la palabra clave override indica que se sobreescribe el metodo ToString</remarks>
-            /// <returns>un string con la fecha en formato dia/mes/anyo</returns>
-            public override string ToString()
-            {
-                return D + "/" + M + "/" + A;
-            }
+        {
+            D = 1;
+            M = 1;
+            A = 1;
         }
-    
+        /// <summary>
+        /// Constructor de Fecha con 3 parámetros
+        /// Si algún parámetro no es correcto se establece a 1
+        /// </summary>
+        /// <param name="mes">Mes</param>
+        /// <param name="anyo">Anyo (entre 1 y 2500)</param>
+        /// <param name="dia">Dia</param>
+        /// <param name="bi">Indica si es bisiesto</param>
+        public Fecha(int mes, int anyo, int dia)
+        {
+            if (anyo >= 1 && anyo <= 2500)
+            {
+                this.A = anyo;
+            }
+            else
+            {
+                this.A = 1;
+            }
+            if ((A % 400 == 0) || ((A % 4 == 0) && (A % 100 != 0)))
+                bisiesto = true;
+            else bisiesto = false;
+            if (mes >= 1 && mes <= 12)
+                this.M = mes;
+            else
+                this.M = 1;
+            int diasMes = 0;
+            switch (M)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    diasMes = 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    diasMes = 30;
+                    break;
+                case 2: // verificación de año bisiesto
+                    if (bisiesto)
+                        diasMes = 29;
+                    else
+                        diasMes = 28;
+                    break;
+            }
+
+            if (dia >= 1 && dia <= diasMes)
+                this.D = dia;
+            else
+                this.D = 1;
+        }
+        public bool EsBisiesto()
+        {
+            if ((A % 400 == 0) || ((A % 4 == 0) && (A % 100 != 0)))
+                bisiesto = true;
+            else
+                bisiesto = false;
+            return bisiesto;
+        }
+        /// <summary>
+        /// Devuelve un string con la fecha en formato dia/mes/anyo
+        /// </summary>
+        /// <remarks> la palabra clave override indica que se sobreescribe el metodo ToString</remarks>
+        /// <returns>un string con la fecha en formato dia/mes/anyo</returns>
+        public override string ToString()
+        {
+            return D + "/" + M + "/" + A;
+        }
+    }
+
 }
